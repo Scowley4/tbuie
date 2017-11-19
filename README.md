682d87e36d3df72d01e4cf8a421efab72f973ea7
Jeffrey Lund <jefflund@gmail.com>
2017-11-18 18:42:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# TBUIE
+
+This is demo of the interactive topic modeling capabilities of
+[ankura](https://github.com/jefflund/ankura).
+
+On the name: I'm not a front end guy. I don't have a clue what I'm doing here.
+This started out as a text box where you entered in raw json into a text box
+and hit a button to replace that json with the results...it was sardonically
+called the best UI ever and the name stuck. To be fair, the UI has come a
+little ways since then, but not much...
---
+++ tbuie.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+import ankura
+import json
+import flask
+
+app = flask.Flask(__name__, static_url_path='')
+
+train_size = 1000
+
+corpus = ankura.corpus.newsgroups()
+Q = ankura.anchor.build_cooccurrence(corpus)
+gs_anchor_indices = ankura.anchor.gram_schmidt_anchors(corpus, Q, 20, return_indices=True)
+gs_anchor_vectors = Q[gs_anchor_indices]
+gs_anchor_tokens = [[corpus.vocabulary[index]] for index in gs_anchor_indices]
+
+@app.route('/')
+def serve_itm():
+    return app.send_static_file('index.html')
+
+@app.route('/vocab')
+def get_vocab():
+    return flask.jsonify(vocab=corpus.vocabulary)
+
+@app.route('/finished', methods=['GET', 'POST'])
+def finish():
+    print('finished')
+
+
+@app.route('/topics')
+def topic_request():
+    raw_anchors = flask.request.args.get('anchors')
+
+    if raw_anchors is None:
+        anchor_tokens, anchor_vectors = gs_anchor_tokens, gs_anchor_vectors
+    else:
+        anchor_tokens = json.loads(raw_anchors)
+        anchor_vectors = ankura.anchor.tandem_anchors(anchor_tokens, Q, corpus)
+
+    topics = ankura.anchor.recover_topics(Q, anchor_vectors)
+    topic_summary = ankura.topic.topic_summary(topics, corpus)
+
+    return flask.jsonify(anchors=anchor_tokens,
+                         topics=topic_summary,
+                         accuracy=.7522883,
+
+                         examples=[],
+                         single_anchors=False)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=5000)
