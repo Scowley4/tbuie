f1c86423b6e175850e00becd540cb128a9731631
Stephen Holmes Cowley <scowley4@roi.aml.cs.byu.edu>
2018-04-23 10:02:19 -0600

    Connected Dream

--- tbuie.py
+++ tbuie.py
@@ -1,30 +1,51 @@
 #!/usr/bin/python3
 
-import ankura
 import json
 import flask
 import random
+import os
+import ankura
+import time
+from tqdm import tqdm
+import sys
 
 app = flask.Flask(__name__, static_url_path='')
 
 attr_name = 'coarse_newsgroup'
 
-def split_train_test(num_docs, train_size=1000, test_size=1000):
+def split_train_test(num_docs, train_size=10000, test_size=200):
     shuffled_doc_ids = list(range(num_docs))
     random.shuffle(shuffled_doc_ids)
     return shuffled_doc_ids[:train_size], set(shuffled_doc_ids[train_size:train_size+test_size])
 
-@ankura.util.pickle_cache('newsgroups.pickle')
+@ankura.util.pickle_cache('newsgroups_train10000_test2000_k50_lw1_smoothing0.pickle')
 def load_data():
+    train_size = 10000
+    test_size = 2000
+    number_of_topics = 50
+    label_weight = 1
+    smoothing = 0
+
+
+    print('***Getting the corpus')
     corpus = ankura.corpus.newsgroups()
-    train_ids, test_ids = split_train_test(len(corpus.documents))
-    Q, labels = ankura.anchor.build_labeled_cooccurrence(corpus, attr_name, train_ids, 500, 1e-5)
-    gs_anchor_indices = ankura.anchor.gram_schmidt_anchors(corpus, Q, 20, return_indices=True)
+
+    split = ankura.pipeline.test_train_split(corpus, num_train=train_size, num_test=test_size, return_ids=True)
+    (train_ids, train_corpus), (test_ids, test_corpus) = split
+
+    Q, labels = ankura.anchor.build_labeled_cooccurrence(corpus, attr_name, train_ids,
+                                                        label_weight=label_weight, smoothing=smoothing)
+
+    gs_anchor_indices = ankura.anchor.gram_schmidt_anchors(corpus, Q, k=number_of_topics, return_indices=True)
+
     gs_anchor_vectors = Q[gs_anchor_indices]
     gs_anchor_tokens = [[corpus.vocabulary[index]] for index in gs_anchor_indices]
-    return corpus, Q, labels, train_ids, test_ids, gs_anchor_vectors, gs_anchor_tokens
+    return corpus, Q, labels, train_ids, train_corpus, test_ids, test_corpus, gs_anchor_vectors, gs_anchor_indices, gs_anchor_tokens
 
-corpus, Q, labels, train_ids, test_ids, gs_anchor_vectors, gs_anchor_tokens = load_data()
+start = time.time()
+corpus, Q, labels, train_ids, train_corpus, test_ids, test_corpus, gs_anchor_vectors, gs_anchor_indices, gs_anchor_tokens = load_data()
+if time.time()-start<5:
+    print('***Loaded the pickle cache')
 
 @app.route('/')
 def serve_itm():
@@ -44,26 +65,53 @@ def finish():
 def topic_request():
     raw_anchors = flask.request.args.get('anchors')
 
+    start=time.time()
     if raw_anchors is None:
         anchor_tokens, anchor_vectors = gs_anchor_tokens, gs_anchor_vectors
     else:
         anchor_tokens = json.loads(raw_anchors)
         anchor_vectors = ankura.anchor.tandem_anchors(anchor_tokens, Q, corpus)
+    print('***tadem_anchors:', time.time()-start)
 
-    topics = ankura.anchor.recover_topics(Q, anchor_vectors)
+    start=time.time()
+    # After change to variational assign, this is most time consuming part
+    # (depending on train_corpus size)
+    C, topics = ankura.anchor.recover_topics(Q, anchor_vectors, get_c=True)
+    print('***recover_topics:', time.time()-start)
+
+    start=time.time()
     topic_summary = ankura.topic.topic_summary(topics[:len(corpus.vocabulary)], corpus)
+    print('***topic_summary:', time.time()-start)
 
-    classifier = ankura.topic.free_classifier(topics, Q, labels)
-    ankura.topic.variational_assign(corpus, topics) # except only do this for test...
+    #print('anchors',anchor_tokens)
+    #print('topics',topic_summary)
+
+    start=time.time()
+    #classifier = ankura.topic.free_classifier_revised(topics, Q, labels)
+
+    ###
+    # FIXME I think this is supposed to use C.T, check with Emily. I have put
+    # in C.T for now
+    ###
+    classifier = ankura.topic.free_classifier_dream(corpus, attr_name, labeled_docs=train_ids, topics=topics, C=C.T, labels=labels)
+    print('***Get Classifier:', time.time()-start)
+    start=time.time()
+
+    ankura.topic.variational_assign(test_corpus, topics)
+
+    print('***variational assign:', time.time()-start)
+
     contingency = ankura.validate.Contingency()
-    for d in test_ids:
-        doc = corpus.documents[d]
+
+    start=time.time()
+    for doc in test_corpus.documents:
         gold = doc.metadata[attr_name]
         pred = classifier(doc)
         contingency[gold, pred] += 1
+    print('***Classify:', time.time()-start)
 
     return flask.jsonify(anchors=anchor_tokens,
-                         topics=topic_summary.tolist(),
+                         topics=topic_summary,
                          accuracy=contingency.accuracy())
 
 if __name__ == '__main__':
